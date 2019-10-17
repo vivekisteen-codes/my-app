bfa9b904e326c8c9c9a480bbcc71889a568e470f
vivek_mt0399 <vivek.gopalan@myntra.com>
2019-10-17 22:40:14 +0530

    Updates

---
+++ precache-manifest.ca56ed4abc205ee5630bb616f903da86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b27e814bb35ccb8e336461671714c1d",
+    "url": "/vivekisteen-codes/my-app/index.html"
+  },
+  {
+    "revision": "a5a2c19d8d8a33f56a60",
+    "url": "/vivekisteen-codes/my-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "fb9b6994be44e562d3ed",
+    "url": "/vivekisteen-codes/my-app/static/js/2.e23ceaee.chunk.js"
+  },
+  {
+    "revision": "a5a2c19d8d8a33f56a60",
+    "url": "/vivekisteen-codes/my-app/static/js/main.09f85582.chunk.js"
+  },
+  {
+    "revision": "e7cfc2276b4908ca1234",
+    "url": "/vivekisteen-codes/my-app/static/js/runtime-main.b1e579c0.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/vivekisteen-codes/my-app/static/media/logo.25bf045c.svg"
+  }
+]);
